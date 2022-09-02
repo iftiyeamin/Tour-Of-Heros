ca2cbd1c984ce08da25c5c91e98e1329a3455325
iftiyeamin <iftiyeamin08@gmail.com>
2022-09-02 19:59:29 +0600

    list added

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    AppComponent,
+    HeroesComponent
+  ],
+  imports: [
+  BrowserModule,
+  FormsModule,
+
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
